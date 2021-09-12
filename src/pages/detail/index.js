541229633b2fdda49d9d4b39cfc6491e64e9fa0f
mikejguo290 <mikejguo290@gmail.com>
2021-09-12 22:58:17 +0100

    comments about the workings of index.js of PetDetailsPage/component.

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
+// make a call to getPetDetails from api/petfinder to get details of one pet. save it to state data and render it. 
+
+// uses loading and error states to handle Promise Lifecycle states. loading=true and error=false by default. 
+// loading is set to false at end of try catch back.
+// see a nested ternary expression, rendering different elements/components on lifecycle state. 
+// loading? loading msg : error? error msg : render pet detaisl 
+
+// again, async function to get data is defined AND executed inside useEffect hook which fires whenever pet id changes. 
+
+
 const PetDetailsPage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
