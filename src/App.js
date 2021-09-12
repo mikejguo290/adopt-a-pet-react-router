d5cfec76858e028e4df2da8a3723e281800ca87d
mikejguo290 <mikejguo290@gmail.com>
2021-09-12 22:16:04 +0100

    comment on the use of index.js to allow importing its parent folder as component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
 
+/*
+An index.js file allow you to import its parent folder as a module. 
+how? - when importing a module, js try to find index.js to import by default. 
+a component exported by default in index.js can be thus imported easily, and given a meaningful, useful name too!
+*/
+
 function App() {
   return (
     <div>
